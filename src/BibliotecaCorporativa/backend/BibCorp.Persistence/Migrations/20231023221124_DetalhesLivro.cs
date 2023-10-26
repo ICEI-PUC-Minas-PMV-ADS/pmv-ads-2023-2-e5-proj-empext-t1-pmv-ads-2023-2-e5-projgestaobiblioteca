a60bf12c75d4ae491694afa54d4dfead55085436
bella <belladamiao2@gmail.com>
2023-10-25 18:51:21 -0700

    detalhes tela

---
+++ src/BibliotecaCorporativa/backend/BibCorp.Persistence/Migrations/20231023221124_DetalhesLivro.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BibCorp.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class DetalhesLivro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comentarios",
+                table: "Acervos",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "QtdPaginas",
+                table: "Acervos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comentarios",
+                table: "Acervos");
+
+            migrationBuilder.DropColumn(
+                name: "QtdPaginas",
+                table: "Acervos");
+        }
+    }
+}
